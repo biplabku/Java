616f3935fba4794557a8add9d6d2da79669eddb3
Biplab Kumar Das <dasbiplabtu@gmail.com>
2017-02-20 23:36:29 -0500

    Update NexthighestNumber.java

--- InterviewQuestion/NexthighestNumber.java
+++ InterviewQuestion/NexthighestNumber.java
@@ -1,31 +1,32 @@
 import java.util.Arrays;
 
+import java.util.Arrays;
+
 /**
  * Created by biplabkumardas on 2/20/17.
  */
 public class highestnumber {
     public static void main(String[] args){
-        String s = "235612443";
-        nexthighest(s);
+        String s = "1";
+        System.out.println(nexthighest(s));
     }
 
     public static String nexthighest(String s){
         int len = s.length();
         int curr = 0;
         int succ = 0;
+
         // find the maximum number in the string and save its next highest number as well as the position
         for(int i = len - 1 ; i >= 1; i--){
             if(Integer.valueOf(s.charAt(i))  > Integer.valueOf(s.charAt(i - 1))){
                 curr = i;
                 succ = i - 1;
-                swap(i, i - 1, s);
-                break;
+                swap(curr, succ, s);
+                return s;
             }
         }
-
-
-
-        return s;
+        String x = "number doesnot exists";
+        return x;
     }
 
     public static void swap(int i, int j, String s){
@@ -44,6 +45,5 @@ public class highestnumber {
         String newstring;
         newstring = String.valueOf(ar);
         s = newstring;
-        System.out.println(s);
     }
 }
