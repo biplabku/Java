cab4e8c9fd6bfa73688a8758814bfb49cb2f82b9
Biplab Kumar Das <dasbiplabtu@gmail.com>
2017-08-09 22:39:10 -0500

    // Better optimized code. Stack for checking the palindrome

--- String Programs/LongestPalindromeBrute.java
+++ String Programs/LongestPalindromeBrute.java
@@ -1,32 +1,33 @@
 
     // find the longest palindrome in the sequence
-    public String longestpalindrome(String str){
-        HashSet<Character> hs = new HashSet<>();
-        hs.add(str.charAt(0));
-        int count = 0;
-        String res = "";
-        String out = "";
-        for(int i = 0; i < str.length()-1; i++){
-            for(int j = i+1; j <= str.length(); j++){
-                res = str.substring(i,j);
-                if(isPalindrome(res)){
-                    if(res.length() > count){
-                        count = res.length();
-                        out = res;
-                    }
-                }
-            }
-        }
-        return out;
-    }
+    public String longestPalindrome(String str){
+		 // It works in O(N3) 
+		 HashMap<Character, Integer> hmap = new HashMap<>();
+		 int res = 0;
+		 String ans = "";
+		 String out = "";
+		 for(int i = 0; i < str.length(); i++){
+			 for(int j = i+1; j <= str.length(); j++){
+				 ans = str.substring(i, j);
+				 if(isPalindrome(ans)){
+					 res = Math.max(res, ans.length());
+					 out = out.length() > ans.length() ? out : ans; // comparison operator without using any loops and stuff
+				 }
+			 }
+		 }
+		 return out;
+	 }
 
     public boolean isPalindrome(String str){
-        String rev = "";
-        for(int i = str.length()-1; i >= 0; i--){
-            rev = rev + str.charAt(i);
-        }
-        if(!str.equals(rev)){
-            return false;
-        }
-        return true;
-    }
+		 Stack<Character> st = new Stack<>();
+		 for(int i = 0 ; i < str.length(); i++){
+			 st.push(str.charAt(i));
+		 }
+		 
+		 for(int i = 0 ; i < str.length(); i++){
+			 if(st.pop() != str.charAt(i)){
+				 return false;
+			 }
+		 }
+		 return true;
+	 }
